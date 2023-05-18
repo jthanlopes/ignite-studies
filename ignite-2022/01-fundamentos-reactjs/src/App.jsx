b117f847949febb4ef47a17cc7c80fb38f96ecef
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-05-18 11:05:18 -0300

    Estrutura da aplicação: componente: Sidebar

--- ignite-2022/01-fundamentos-reactjs/src/App.jsx
+++ ignite-2022/01-fundamentos-reactjs/src/App.jsx
@@ -1,22 +1,30 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Jonathan dos Santos"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum exercitationem maiores repellat dicta nihil incidunt culpa id in ex mollitia."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
 
-      <Post
-        author="Cinder Lopes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum at, soluta earum cupiditate nulla fuga magnam accusantium asperiores tempora debitis aperiam odio? Illum, optio exercitationem. Laudantium aspernatur cum commodi odio harum quam numquam? Cupiditate laudantium sapiente dolor quibusdam numquam. Ipsum, dolor. Recusandae voluptate aspernatur optio repudiandae aliquid fugiat nesciunt dolores."
-      />
+        <main>
+          <Post
+            author="Jonathan dos Santos"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum exercitationem maiores repellat dicta nihil incidunt culpa id in ex mollitia."
+          />
+
+          <Post
+            author="Cinder Lopes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum at, soluta earum cupiditate nulla fuga magnam accusantium asperiores tempora debitis aperiam odio? Illum, optio exercitationem. Laudantium aspernatur cum commodi odio harum quam numquam? Cupiditate laudantium sapiente dolor quibusdam numquam. Ipsum, dolor. Recusandae voluptate aspernatur optio repudiandae aliquid fugiat nesciunt dolores."
+          />
+        </main>
+      </div>
     </div>
   )
 }
