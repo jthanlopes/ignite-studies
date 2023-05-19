163aa4debe64e539872be25b270425037839f1af
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-05-19 17:55:42 -0300

    Estrutura da aplicação: componente: Post

---
+++ ignite-2022/01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css'
+
+export function Post(props) {
+  console.log(props);
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/17733564?v=4"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Jonathan dos Santos</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="19 de Maio às 17:14h" dateTime="2023-05-19 17:13:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉<a href="#"> jane.design/doctorcare</a></p>
+        <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
