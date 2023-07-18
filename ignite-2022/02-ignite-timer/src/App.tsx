71a477b54252826f3f80ebf92a97b619eb127ba7
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-07-18 17:11:13 -0300

    Estrutura da aplicação: estilos globais

--- ignite-2022/02-ignite-timer/src/App.tsx
+++ ignite-2022/02-ignite-timer/src/App.tsx
@@ -1,7 +1,9 @@
 import { ThemeProvider } from 'styled-components';
 
 import { Button } from "./components/Button";
+
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
@@ -11,6 +13,8 @@ export function App() {
       <Button variant="success"/>
       <Button variant="danger"/>
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
