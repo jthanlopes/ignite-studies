c6561ddd8b8ca1b0d21cacb737711ab2c107da87
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-07-20 14:01:01 -0300

    Páginas e rotas: React Router DOM

--- ignite-2022/02-ignite-timer/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './Buttom.styles'
-
-interface ButtonProps {
-  variant?: ButtonVariant
-}
-
-export const Button = ({ variant = 'primary' }: ButtonProps): JSX.Element => {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
