c5524dfc86fdb8f25f0961a4ddd80ee6fd6b3617
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-09-21 08:37:56 -0300

    Estrutura visual: botões de entrada e saída

--- ignite-2022/03-dt-money/src/components/NewTransactionModal/styles.ts
+++ ignite-2022/03-dt-money/src/components/NewTransactionModal/styles.ts
@@ -51,11 +51,40 @@ export const Content = styled(Dialog.Content)`
       cursor: pointer;
 
       &:hover {
-
+        background: ${props => props.theme['green-700']};
+        transition: background-color 0.2s;
       }
     }
   }
 `;
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`;
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome';
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${props => props.theme['green-700']};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  background: ${props => props.theme['gray-300']};
+
+  svg {
+    color: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-300']};
+  }
+`
 
 export const CloseButton = styled(Dialog.Close)`
   position: absolute;
