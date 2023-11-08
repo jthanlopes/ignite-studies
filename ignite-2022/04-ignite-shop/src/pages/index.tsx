44786ed98d82851e83e0403e220b97254b53ac0f
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-11-08 16:41:39 -0300

    Estrutura visual: criando carrossel

--- ignite-2022/04-ignite-shop/src/pages/index.tsx
+++ ignite-2022/04-ignite-shop/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { useKeenSlider } from 'keen-slider/react'
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import { styled } from "../styles"
@@ -6,11 +7,21 @@ import { styled } from "../styles"
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
+
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="Camiseta 1" />
 
         <footer>
@@ -19,12 +30,30 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="Camiseta 2" />
 
         <footer>
           <strong>Camiseta Y</strong>
           <span>R$ 89,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="Camiseta 3" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 119,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} width={520} height={480} alt="Camiseta 4" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 129,00</span>
         </footer>
       </Product>
     </HomeContainer>
